65b3ec2b60e79a43ad5555ce70234340fb98e456
Irma <irblancofdz@gmail.com>
2018-04-02 15:22:44 +0200

    Add first constraints

--- EverPobre/AppDelegate.swift
+++ EverPobre/AppDelegate.swift
@@ -15,7 +15,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow()
+        
+        let noteVC = NoteViewController()
+        
+        window?.rootViewController = noteVC
+        
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
